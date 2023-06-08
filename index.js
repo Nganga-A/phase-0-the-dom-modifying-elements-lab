f8ddbdcab8ec27a361b37150bfd3ef51ca8c32aa
Abed Nganga <ngangaabed@gmail.com>
2023-06-08 22:31:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+document.querySelector ('#main').remove(); //remove main
+
+const newHeader = document.createElement('h1');
+
+newHeader.id ='victory';
+
+const yourName = 'John'; 
+newHeader.textContent = `${yourName} is the champion`;
+
+document.body.appendChild(newHeader);
